428a9b076e8efaf3eb11d734ab4f7cf9ddb740ea
FurkanATR <FurkanATR23@gmail.com>
2023-02-05 23:46:17 +0300

    JKphobic

---
+++ Take-a-Ten-Minutes-Walk.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+// input is a null-terminated string
+
+bool isValidWalk(const char *walk) {
+    int i;
+    char loc[] = {0,0};
+  
+    i = 0;
+    while (walk[i])
+      {
+        if (walk[i] == 'n')
+          loc[0]++;
+        else if (walk[i] =='s')
+          loc[0]--;
+        else if (walk[i] == 'e')
+          loc[1]++;
+        else if (walk[i] == 'w')
+          loc[1]--;
+        i++;
+      }
+    if (loc[0]+loc[1] == 0)
+      return true;
+    else
+      return false;
+}
